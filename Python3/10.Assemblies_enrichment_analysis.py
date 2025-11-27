d204c85337e7b101b6cb10e7fb14118f1a50012d
Lim <76652975+Lim-Bioinfo@users.noreply.github.com>
2025-11-28 03:21:01 +0900

    Update 10.Assemblies_enrichment_analysis.py

--- Python3/10.Assemblies_enrichment_analysis.py
+++ Python3/10.Assemblies_enrichment_analysis.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import scipy.stats as stats
+import gseapy as gp
+
+from NeST import load_hierarchy
 
 if __name__=="__main__":
     print("Open file")
@@ -17,45 +20,19 @@ if __name__=="__main__":
             continue
 
     nest_dict = dict()
-    sasr_set = set([pair[0] for pair in input_pair]) | set([pair[1] for pair in sam_dict['SKCM']])
+    sam_set = set([pair[0] for pair in sam_dict['SKCM']) | set([pair[1] for pair in sam_dict['SKCM']])
 
     for i in range(0, len(nest)):
         if nest.iloc[i, 2] not in nest_dict.keys():
             nest_dict[nest.iloc[i, 2]] = set(nest.iloc[i, 3].split(' '))
 
-    results = []
-    for ctype_genes in [sasr_set]:
-        M = len(set.union(*nest_dict.values()))  # total unique genes across all assemblies
-        if M < len(ctype_genes):
-            # Or you can set M = big number of known human genes if you prefer,
-            # but let's assume union of all NeST genes for demonstration
-            M = len(set.union(*nest_dict.values()))
-    
-        # Alternatively, if you want to define M as e.g. 20k for typical coding genes, do so.
-    
-        for assembly_name, assembly_genes in nest_dict.items():
-            # Overlap
-            overlap_genes = ctype_genes.intersection(assembly_genes)
-            k = len(overlap_genes)   # number of genes from ctype set that are in this assembly
-            n = len(assembly_genes)  # size of the assembly
-            N = len(ctype_genes)     # size of the ctype gene set
-        
-            # Hypergeometric p-value: P(X>=k) for X ~ Hypergeom(M, n, N)
-            # But we often do a one-sided test for enrichment => sf(k-1,...)
-            pval = stats.hypergeom.sf(k-1, M, n, N)
-
-            results.append({
-                'CancerType': cancer_type,
-                'Assembly': assembly_name,
-                'OverlapCount': k,
-                'AssemblySize': n,
-                'CancerGeneSetSize': N,
-                'TotalGenes': M,
-                'pval': pval,
-                'OverlapGenes': ' '.join(list(overlap_genes))
-            })
-
-    results_df = pd.DataFrame(results)
-    results_df.to_csv("../Result/NeST_SAM_SKCM_enrichment_result.csv", index = False)
+    enr_bg = gp.enrichr(gene_list=list(gene_set),
+                        gene_sets=nest_dict,
+                        organism='human',
+                        outdir=None,
+                        #background= all_genes, #all_genes #'hsapiens_gene_ensembl', 'nest_dict['NeST']'
+                        verbose=False)
+    enr_bg.results.to_csv("Functional_enrichment_NeST_EnrichR_GSEAPY.csv",index = False)
    
+
 
